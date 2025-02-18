48e8eb55e4bb5409a806796a67c4470005c443fe
ccrd1711 <ccrd17@gmail.com>
2025-02-18 21:18:12 +0000

    added all questions planned for quiz at this point

---
+++ assets/js/main.js
@@ -0,0 +1,98 @@
+const questions = [
+    {
+        question: "What division are the Detroit Lions and Minnesota Vikings in?",
+        options: ["AFC North", "NFC North", "AFC South", "NFC South"],
+        correctAnswer: 2
+    },
+
+    { 
+        question: "What division are the New England Patriots and Miami Dolphins in?",
+        options: ["AFC East", "NFC East", "AFC West", "NFC West"],
+        correctAnswer: 0
+    }, 
+
+    { 
+        question: "What division are the Dallas Cowboys and Philadelphia Eagles in?",
+        options: ["AFC East", "NFC East", "AFC West", "NFC West"],
+        correctAnswer: 1
+    }, 
+
+    { 
+        question: "What division are the San Francisco 49ers and Seattle Seahawks in?",
+        options: ["AFC East", "NFC East", "AFC West", "NFC West"],  
+        correctAnswer: 3
+    }, 
+
+    { 
+        question: "What division are the Kansas City Chiefs and Los Angeles Chargers in?",
+        options: ["AFC East", "NFC East", "AFC West", "NFC West"],
+        correctAnswer: 2
+    }, 
+
+    {
+        question: "What division are the New Orleans Saints and Tampa Bay Buccaneers in?",
+        options: ["AFC North", "NFC North", "AFC South", "NFC South"],
+        correctAnswer: 3
+    }, 
+
+    {
+        question: "What division are the Baltimore Ravens and Pittsburgh Steelers in?",
+        options: ["AFC North", "NFC North", "AFC South", "NFC South"],
+        correctAnswer: 0
+    }, 
+
+    {   
+        question: "What division are the Los Angeles Rams and Arizona Cardinals in?",
+        options: ["AFC North", "NFC North", "AFC West", "NFC West"],
+        correctAnswer: 3
+    },
+
+    {
+        question: "What division are the Indianapolis Colts and Houston Texans in?",
+        options: ["AFC North", "NFC North", "AFC South", "NFC South"],
+        correctAnswer: 2
+    },
+
+    {
+        question: "What division are the Buffalo Bills and New York Jets in?",
+        options: ["AFC East", "NFC East", "AFC West", "NFC West"],
+        correctAnswer: 0
+    },
+
+    {
+        question: "What division are the Green Bay Packers and Chicago Bears in?",
+        options: ["AFC North", "NFC North", "AFC South", "NFC South"],
+        correctAnswer: 1
+    },
+
+    {
+        question: "What division are the Tennessee Titans and Jacksonville Jaguars in?",
+        options: ["AFC North", "NFC North", "AFC South", "NFC South"],
+        correctAnswer: 2
+    },
+
+    {
+        question: "What division are the Denver Broncos and Las Vegas Raiders in?",
+        options: ["AFC East", "NFC East", "AFC West", "NFC West"],
+        correctAnswer: 2
+    },
+
+    {
+        question: "What division are the Washington Football Team and New York Giants in?",
+        options: ["AFC East", "NFC East", "AFC West", "NFC West"],
+        correctAnswer: 1
+    },
+
+    {
+        question: "What division are the Atlanta Falcons and Carolina Panthers in?",
+        options: ["AFC North", "NFC North", "AFC South", "NFC South"],
+        correctAnswer: 3
+    },
+
+    {
+        question: "What division are the Cincinnati Bengals and Cleveland Browns in?",
+        options: ["AFC North", "NFC North", "AFC South", "NFC South"],
+        correctAnswer: 0
+    },
+
+]
